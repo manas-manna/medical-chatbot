6a6e3f7e40b3aaae80152e2639ac722ae8954f73
manas-manna <madanmanna123456@gmail.com>
2025-04-10 17:52:12 +0530

    updated folder structure

--- expense-tracker-backend/src/test/java/com/smartfinance/expensetracker/ExpenseTrackerApplicationTests.java
+++ expense-tracker-backend/src/test/java/com/smartfinance/expensetracker/ExpenseTrackerApplicationTests.java
@@ -1,6 +1,6 @@
 package com.smartfinance.expensetracker;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
